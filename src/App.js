222e69ca71a71d7b8107564f2ee1d43ac37b3fd5
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-21 22:32:52 +0200

    Exercise B - Paasing Functions as Props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import HelloWorld from "./HelloWorld";
 import Logo from "./Logo";
 import BestPokemon from "./BestPokemon";
 
+const logWhenClicked = () => (
+  console.log("You clicked the logo")
+)
+
 function App() {
   return (
     <div >
-      <Logo appName="Pokedex"/>
+      <Logo handleClick={logWhenClicked} appName="Pokedex"/>
       <BestPokemon abilities={['Anticipation', 'Adaptability', 'Run-Away']}/>
       <CaughtPokemon date={new Date().toLocaleDateString()}/>
       <HelloWorld />
--- src/Logo.js
+++ src/Logo.js
@@ -1,19 +1,13 @@
 import React from 'react'
 
-const logWhenClicked = () => (
-  console.log("You clicked the logo")
-)
 
-
-//  Traditional Function declaration.
-const Logo = (props) => {
-  console.log("Logo:", props)
-  return (
+const Logo = (props) => (
+  (
     <header>
       <h1>Welcome to {props.appName} Pokedex</h1>
-      <img onClick={logWhenClicked} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
+      <img onClick={props.handleClick} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
     </header>
   )
-}
+)
 
 export default Logo
