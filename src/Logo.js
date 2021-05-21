06194c61e4b175f5f63976ed975d404792196c4c
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-21 22:34:14 +0200

    remove hardcoded Pokedex

--- src/Logo.js
+++ src/Logo.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Logo = (props) => (
   (
     <header>
-      <h1>Welcome to {props.appName} Pokedex</h1>
+      <h1>Welcome to {props.appName} </h1>
       <img onClick={props.handleClick} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
     </header>
   )
