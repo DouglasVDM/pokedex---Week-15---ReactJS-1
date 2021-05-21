1b0855c9144d83cb6186e3fa22571fbab2d71442
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-21 22:13:56 +0200

    onClick with callback function

--- src/Logo.js
+++ src/Logo.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
+const logWhenClicked = () => (
+  console.log("You clicked the logo")
+)
+
 //  Traditional Function declaration.
 const Logo = (props) => {
   console.log("Logo:", props)
   return (
     <header>
       <h1>Welcome to {props.appName} Pokedex</h1>
-      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
+      <img onClick={logWhenClicked} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
     </header>
   )
 }
