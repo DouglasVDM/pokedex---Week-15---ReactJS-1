e87f84c128657b51c4e6611f2058b4120585f0a7
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-30 18:00:12 +0200

    Exercises G - Type name of pokemon and add to list

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,15 +1,19 @@
 import React, { useState }from 'react'
 
 function CaughtPokemon(props) {
+
+  const [pokemonNameInput, setPokemonNameInput] = useState("");
   
   const initialState = [];
-  const [caught, setCaught] = useState(initialState)
+  const [caught, setCaught] = useState(initialState);
 
   let listOfPokemons = ["Pikachu", "Charizard", "Landorus", "Eevee", "Snorlax", "MewTwo", "Charmander", "Ditto", "Squirtle", "Lucario", "Gardevior", "Gengar", "Bulbasaur"];
 
   const randomPokemon = listOfPokemons[Math.floor(Math.random() * listOfPokemons.length)];
   
-  const catchPokemon = () => setCaught(caught.concat(randomPokemon))
+  const catchPokemon = () => setCaught(caught.concat(pokemonNameInput), setPokemonNameInput(""));
+
+  const handleInputChange = (event) => setPokemonNameInput(event.target.value)
 
   return (
     <div>
@@ -24,7 +28,13 @@ function CaughtPokemon(props) {
         }
       </ul>
       <p>Today's date in Cape Town is: {props.date}</p>
-      <button onClick={catchPokemon}>Add Pokemon</button>
+      <input
+        type="text"
+        placeholder="catch a pokemon..."
+        value={pokemonNameInput}
+        onChange={handleInputChange}
+      />
+      <button type="reset" onClick={catchPokemon}>Catch Pokemon</button>
     </div>    
   )
 }
